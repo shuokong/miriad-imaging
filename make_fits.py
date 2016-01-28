d16ce7fd150b62172c79ccb64aa095c86fd6f598
jrobbfed <jrobbfed@gmail.com>
2016-01-28 09:14:38 -0500

    Run mir2fits when make_fits is run on its own

--- make_fits.py
+++ make_fits.py
@@ -1,2 +1,21 @@
 #! usr/bin/env python
+def mir2fits(pattern='*'):
+    """
+    Apply the regex in `pattern` to generate fits files for the corresponding MIRIAD files.
+    """
+    import glob.glob
+    import os
+    for f in glob(pattern):
+        os.system('fits in={0} out={1] op=xyout'.format(f, f))
 
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parse.add_argument("-pattern", type=str, default='*')
+    args = parser.parse_args()
+    mir2fits(pattern=args.pattern)
+
+if __name__ == "__main__":
+    #print("This program is running by itself")
+    main()
